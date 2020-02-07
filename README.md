b0975189fc56bc770c89a7692fb4b28606c1c112
Maxime Thiebaut <46688461+0xThiebaut@users.noreply.github.com>
2020-02-07 15:43:39 +0100

    Initial public release

---
+++ README.md
@@ -0,0 +1,14 @@
+# CTFd-TheHive
+A Cortex responder to award/penalize CTFd players based on the quality of the alerts they generate in TheHive.
+
+## TheHive
+This reponder introduces the ability award and penalize CTFd players for the alerts generated in TheHive.
+
+***Requirement:** The CTFd player's ID is expected to be in the alert's "source" field.*
+
+![TheHive](thumnail-thehive.png)
+
+## CTFd
+In CTFd, the player will be granted awards (positives or negatives) based on the used responder and alert severity.
+
+![CTFd](thumbnail-ctfd.png)
---
+++ ctfd-thehive.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+from urllib import parse
+
+import requests
+from cortexutils.responder import Responder
+
+severities = {1: "low", 2: "medium", 3: "high"}
+
+
+class CTFdTheHive(Responder):
+    def __init__(self):
+        Responder.__init__(self)
+        self.session = self.get_param(
+            'config.session', None, 'Missing CTFd session token')
+        self.csrf = self.get_param(
+            'config.csrf', None, 'Missing CTFd CSRF token')
+        self.api = self.get_param(
+            'config.api', 'https://localhost/api/v1/')
+        self.action = self.get_param(
+            'config.action', None, 'Missing action')
+        self.low = self.get_param(
+            'config.low', 10)
+        self.medium = self.get_param(
+            'config.medium', 25)
+        self.high = self.get_param(
+            'config.high', 100)
+        self.name = self.get_param(
+            'config.award.name', 'Enlightened')
+        self.category = self.get_param(
+            'config.award.category', 'Monitoring')
+        self.icon = self.get_param(
+            'config.award.icon')
+
+    def run(self):
+        Responder.run(self)
+
+        # Get some values from the alert
+        title = self.get_param('data.title', None, 'Missing title for alert')
+        source = self.get_param('data.source', None, 'Missing source for alert')
+        reference = self.get_param('data.sourceRef', None, 'Missing type for alert')
+        severity = self.get_param('data.severity', None, 'Missing type for alert')
+
+        # Interpret the CTFd user identifier
+        try:
+            user = int(source)
+        except ValueError:
+            self.error(f'The source "{source}" does not seem like a valid CTFd user identifier')
+            user = -1
+
+        # Compute the reward...
+        if severity == 3:
+            value = self.high
+        elif severity == 2:
+            value = self.medium
+        else:
+            value = self.low
+
+        # ...and make it negative if we penalize
+        if self.action == "penalize":
+            value = -value
+            state = "incorrectly"
+        else:
+            state = "successfully"
+
+        # Generate the reward
+        desc = f'The "{title}" alert {state} detected a {severities[severity]} severity event (#{reference}).'
+        award = {
+            'user_id': user,
+            'name': self.name,
+            'description': desc,
+            'category': self.category,
+            'icon': self.icon,
+            'value': value
+        }
+
+        # Send the reward
+        endpoint = parse.urljoin(self.api, 'awards')
+        cookies = dict(session=self.session)
+        headers = {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'CSRF-Token': self.csrf
+        }
+        r = requests.post(endpoint, json=award, cookies=cookies, headers=headers)
+
+        # Interpret the response
+        if r.ok:
+            self.report({'award': award, 'response': r.text})
+        else:
+            self.error(f'An error occurred while contacting CTFd at {endpoint} ({r.status_code} {r.reason}): {r.text}')
+
+    def operations(self, raw):
+        return [self.build_operation('MarkAlertAsRead'), self.build_operation('AddTagToCase', tag=self.action)]
+
+
+if __name__ == "__main__":
+    CTFdTheHive().run()
